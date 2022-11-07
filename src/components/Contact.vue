22d2d82f33e30a9b6075e46f38cacb49cba1eb91
Juri Berlanda <juriberlanda@hotmail.com>
2022-11-07 23:23:25 +0100

    [Conact] Fix links.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -26,7 +26,7 @@ const phoneNumber = "+39 339 123 45 67";
                             <i class="fas fa-envelope text-primary mb-2"></i>
                             <h4 class="text-uppercase m-0">Email</h4>
                             <hr class="my-4 mx-auto" />
-                            <div class="small text-black-50"><a href="#!">plattner.tobias@gmail.com</a></div>
+                            <div class="small text-black-50"><a href="mailto:plattner.tobias@gmail.com">plattner.tobias@gmail.com</a></div>
                         </div>
                     </div>
                 </div>
@@ -37,7 +37,7 @@ const phoneNumber = "+39 339 123 45 67";
                             <h4 class="text-uppercase m-0">Telefon</h4>
                             <hr class="my-4 mx-auto" />
                             <div class="small text-black-50">
-                                <a href="tel:+393391234567">+39 348 544 59 41</a>
+                                <a href="tel:+393485445941">+39 348 544 59 41</a>
                             </div>
                         </div>
                     </div>
